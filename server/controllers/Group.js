872848758bb00752545f6d6a0484f6c453d6ff9c
charpell <charpellumeh@gmail.com>
2017-09-08 21:16:31 +0100

    Refactor User.js

--- server/controllers/Group.js
+++ server/controllers/Group.js
@@ -8,17 +8,17 @@ class Group {
   static createGroup(req, res) {
     const { groupName, userName } = req.body;
     if (!(validStringLength(userName, groupName) && validStringContent(userName, groupName))) {
-        res.status(400).json({ message: 'The Username or Groupname field is invalid' });
-      } else {
-        const db = firebase.database();  
-        groupRef.child(groupName).once('value', (snapshot) => {
-        if (!snapshot.exists()) { 
-     // Create  and Group and Insert Username
-            groupRef.child(groupName).child('Users').child(userName).set(userName)
-            groupRef.child(groupName).child('Users').child('Bot').set('Bot')
-            groupRef.child(groupName).child('Email').push('bot@postit.com')
-            groupRef.child(groupName).child('Number').push('2348066098146')  
-  
+      res.status(400).json({ message: 'The Username or Groupname field is invalid' });
+    } else {
+      const db = firebase.database();
+      groupRef.child(groupName).once('value', (snapshot) => {
+        if (!snapshot.exists()) {
+          // Create  and Group and Insert Username
+          groupRef.child(groupName).child('Users').child(userName).set(userName)
+          groupRef.child(groupName).child('Users').child('Bot').set('Bot')
+          groupRef.child(groupName).child('Email').push('bot@postit.com')
+          groupRef.child(groupName).child('Number').push('2348066098146')
+
           //Push the user's details into Group/ Users
           db.ref(`/users/${userName}/Groups`).push({
             groupName,
@@ -32,19 +32,19 @@ class Group {
           res.status(409).json({ message: 'Group already exists' });
         }
       }).catch((err) => {
-          res.status(401).json({ message: `Something went wrong ${err.message}` });
-        });
+        res.status(401).json({ message: `Something went wrong ${err.message}` });
+      });
 
-      }
-   
     }
+
+  }
 
   static addUserToGroup(req, res) {
     const { groupName, user } = req.body;
     if (!(validStringLength(groupName, user) && validStringContent(groupName, user))) {
       res.status(400).json({ message: 'The Username or Groupname field is invalid' });
     } else {
-      const db = firebase.database();   
+      const db = firebase.database();
       usersRef.child(user).once('value', (snapshot) => {
         if (snapshot.exists()) {
           const username = snapshot.val().username;
@@ -53,59 +53,59 @@ class Group {
           //Push the user's details into Group/ Users
           db.ref(`/users/${user}/Groups`).push({
             groupName
-          });  
+          });
 
           //Push the user's details into Group but first check if the group exist
-          groupRef.child(groupName).once('value', (groupSnapshot) => {                 
-              if (groupSnapshot.exists()) { 
-                groupRef.child(groupName).child('Users').child(username).set(username)
-                groupRef.child(groupName).child('Email').push(email)
-                groupRef.child(groupName).child('Number').push(number)     
-              } else {
-                res.status(403).json({ message: "Group dosen't exists" });
-              }     
-          })        
-          .then(() => {
+          groupRef.child(groupName).once('value', (groupSnapshot) => {
+            if (groupSnapshot.exists()) {
+              groupRef.child(groupName).child('Users').child(username).set(username)
+              groupRef.child(groupName).child('Email').push(email)
+              groupRef.child(groupName).child('Number').push(number)
+            } else {
+              res.status(403).json({ message: "Group dosen't exists" });
+            }
+          })
+            .then(() => {
               res.status(201).json({ message: 'User added successfully' });
-          });    
+            });
         } else {
           res.status(403).json({ message: "The User dosen't exist" })
-        }   
+        }
       })
-      .catch((error) => {
-        res.status(500).json({ message: error });
-      });  
-      }
+        .catch((error) => {
+          res.status(500).json({ message: error });
+        });
+    }
 
-}
+  }
 
 
   static createMessage(req, res) {
     const { groupName, messages, notification, priority } = req.body;
 
-    
-    if (!(validStringLength(groupName, messages) )) {
+
+    if (!(validStringLength(groupName, messages))) {
       res.status(400).json({ message: 'The Message or Groupname field is invalid' });
-    } else if (!(validStringLength(notification, priority) )) {
+    } else if (!(validStringLength(notification, priority))) {
       res.status(400).json({ message: 'The Notification or Priority field is invalid' });
     } else {
-    const db = firebase.database();
-    firebase.auth().onAuthStateChanged((user) => {
+      const db = firebase.database();
+      firebase.auth().onAuthStateChanged((user) => {
         if (user) {
           const userName = user.displayName;
           console.log(userName)
-				groupRef.child(groupName).child("Messages").push(
-        {  
-          user: userName,
-          Message: messages,
-          Priority: priority,
-        })
+          groupRef.child(groupName).child("Messages").push(
+            {
+              user: userName,
+              Message: messages,
+              Priority: priority,
+            })
 
-        .then(() => {
-          res.status(201).json({ message: 'Message added successfully'});
-          }).catch((error) => {
-          res.status(500).send(error);
-          });
+            .then(() => {
+              res.status(201).json({ message: 'Message added successfully' });
+            }).catch((error) => {
+              res.status(500).send(error);
+            });
 
           // Notify Every User for Notification
           const users = []
@@ -115,14 +115,14 @@ class Group {
             .child(groupName)
             .child('Users');
           userRef.once('value', snap => {
-          let user = {}
-          snap.forEach((data) => {
-            users.push(data.val())
-            })   
+            let user = {}
+            snap.forEach((data) => {
+              users.push(data.val())
+            })
             users.forEach((entry) => {
-              const db = firebase.database(); 
+              const db = firebase.database();
               db.ref(`/users/${entry}/Notifications`).child(notification).set(notification);
-           }) 
+            })
           })
 
           // Send an Email to every User in the Group
@@ -138,31 +138,31 @@ class Group {
             });
             const emails = email.join(',');
 
-            if((priority === 'Urgent') || (priority === 'Critical')){
+            if ((priority === 'Urgent') || (priority === 'Critical')) {
               // Send Email Notification to Users
-           let transporter = nodemailer.createTransport(smtpTransport({
-           service: "gmail",
-           auth: {
-               user: 'wesumeh@gmail.com',
-               pass: 'dericoderico'
-           }
-           }));
-           let mailOptions = {
-               from: '"PostIt App" <admin@postit.com>', // sender address
-               to: emails, // list of receivers
-               subject: 'New Message Received', // Subject line
-               text: 'PostIt App ?', // plain text body
-               html: '<p>Hello</p><h2>This is to notify you that a vey urgent message which may have a critical priority level has been posted in '+ groupName +' group</h2>' // html body
-           };
-           transporter.sendMail(mailOptions, (error, info) => {
-               if (error) {
-                console.log(error);
-               }
+              let transporter = nodemailer.createTransport(smtpTransport({
+                service: "gmail",
+                auth: {
+                  user: 'wesumeh@gmail.com',
+                  pass: 'dericoderico'
+                }
+              }));
+              let mailOptions = {
+                from: '"PostIt App" <admin@postit.com>', // sender address
+                to: emails, // list of receivers
+                subject: 'New Message Received', // Subject line
+                text: 'PostIt App ?', // plain text body
+                html: '<p>Hello</p><h2>This is to notify you that a vey urgent message which may have a critical priority level has been posted in ' + groupName + ' group</h2>' // html body
+              };
+              transporter.sendMail(mailOptions, (error, info) => {
+                if (error) {
+                  console.log(error);
+                }
                 console.log('Message %s sent: %s', info);
-           });
-         }
+              });
+            }
           });
-          
+
 
           const number = [];
           const numberRef = firebase.database()
@@ -174,73 +174,73 @@ class Group {
             snap.forEach((data) => {
               number.push(data.val());
             });
-            if(priority === 'Critical'){
+            if (priority === 'Critical') {
               //Send SMS Notification to Users in a particular Group
               const nexmo = new Nexmo({
-              apiKey: '47f699b7',
-              apiSecret: 'ebc6283d134add6e'
-            });
-            //Loop through the numbers and send sms per each number
+                apiKey: '47f699b7',
+                apiSecret: 'ebc6283d134add6e'
+              });
+              //Loop through the numbers and send sms per each number
               number.forEach((entry) => {
                 nexmo.message.sendSms(
-                  'Post-It', entry, 'Post-It App. This is to notify you that an urgent message which has a critical priority level has been posted in '+ groupName +' group',
-                    (error, responseData) => {
-                      if (error) {
-                        console.log(error);
-                      } else {
-                        console.log(responseData);
-                      }
+                  'Post-It', entry, 'Post-It App. This is to notify you that an urgent message which has a critical priority level has been posted in ' + groupName + ' group',
+                  (error, responseData) => {
+                    if (error) {
+                      console.log(error);
+                    } else {
+                      console.log(responseData);
                     }
+                  }
                 );
+              });
+            }
           });
-          }
+        } else {
+          res.status(403).send({
+            message: 'You are not signed in right now!'
           });
-      } else {
-        res.status(403).send({
-          message: 'You are not signed in right now!'
-        });
-      }
-    });
+        }
+      });
+    }
   }
-	}
-  
+
 
   static usersAndMessagesInGroups(req, res) {
     const groupName = req.params.groupName;
     const messages = []
     const users = []
-    
+
     const messageRef = firebase.database()
-    .ref()
-    .child('Groups')
-    .child(groupName)
-    .child('Messages');
+      .ref()
+      .child('Groups')
+      .child(groupName)
+      .child('Messages');
     messageRef.once('value', (snap) => {
-    let message = {}
-    snap.forEach((data) => {    
-      message = {
-        id: data.key,
-        user: data.val().user,
-        text: data.val().Message,
-        Priority: data.val().Priority
-      }
-      messages.push(message)
+      let message = {}
+      snap.forEach((data) => {
+        message = {
+          id: data.key,
+          user: data.val().user,
+          text: data.val().Message,
+          Priority: data.val().Priority
+        }
+        messages.push(message)
       })
 
       const userRef = firebase.database().ref().child('Groups').child(groupName).child('Users');
       userRef.once('value', snap => {
-      let user = {}
-      snap.forEach((data) => {
-        user = {
-          userName: data.val()
-        }
-        users.push(user)
-        })   
+        let user = {}
+        snap.forEach((data) => {
+          user = {
+            userName: data.val()
+          }
+          users.push(user)
+        })
 
         res.status(200).json({
           message: `Getting Messages and Users in ${groupName} database`,
-          messages, 
-          users     
+          messages,
+          users
         });
       })
 
@@ -248,29 +248,29 @@ class Group {
 
 
     const messageIDRef = firebase.database()
-    .ref()
-    .child('Groups')
-    .child(groupName)
-    .child('Messages');
+      .ref()
+      .child('Groups')
+      .child(groupName)
+      .child('Messages');
     messageIDRef.once('value', (snap) => {
-    const messageIDs = []  
-    snap.forEach((data) => {    
-      messageIDs.push(data.key)
+      const messageIDs = []
+      snap.forEach((data) => {
+        messageIDs.push(data.key)
       })
       // Push the Usernmame into the list of Messages
       messageIDs.forEach((entry) => {
-        const db = firebase.database(); 
+        const db = firebase.database();
         db.ref(`/Groups/groupName/Messages/${entry}`).child('Seen').push('Tam');
-    })
+      })
 
-    // const db = firebase.database(); 
-    // db.ref('/Groups/groupName/Messages/-KtSBsEbClNIJFNAdm_K').child('Seen').push('Tam');
-  })
+      // const db = firebase.database(); 
+      // db.ref('/Groups/groupName/Messages/-KtSBsEbClNIJFNAdm_K').child('Seen').push('Tam');
+    })
 
 
   }
 
-  
+
 
 }
 
--- server/controllers/User.js
+++ server/controllers/User.js
@@ -1,18 +1,18 @@
 const { firebase, usersRef, groupRef } = require('../config');
 /** Class representing a the User Database. */
 class User {
- /**
-     * The Sign Up method
-     * @param {string} req - User's Request
-     * @param {object} res - Server Response
-     * @return {object}  returns the user's details
-     */
+  /**
+      * The Sign Up method
+      * @param {string} req - User's Request
+      * @param {object} res - Server Response
+      * @return {object}  returns the user's details
+      */
   static signup(req, res) {
     const { userName, password, email, number } = req.body;
 
     if (typeof userName === 'undefined' || typeof email === 'undefined' ||
-        typeof password === 'undefined' || typeof number === 'undefined') {
-      res.status(400).json({ 
+      typeof password === 'undefined' || typeof number === 'undefined') {
+      res.status(400).json({
         message: 'You need to provide userName, password, number and email',
       });
     } else if (userName === '' || password === '' || email === '' || number === '') {
@@ -33,26 +33,26 @@ class User {
           uid,
           number
         });
-      })   
-    .catch((error) => {
-      const errorCode = error.code;
-      if (errorCode === 'auth/invalid-email') {
-        res.status(400).json({ message: error.message });
-      } else if (errorCode === 'auth/weak-password') {
-        res.status(400).json({ message: error.message });         
-      } else if (errorCode === 'auth/email-already-in-use') {
-        res.status(409).json({ message: error.message });
-      }
-    });
+      })
+        .catch((error) => {
+          const errorCode = error.code;
+          if (errorCode === 'auth/invalid-email') {
+            res.status(400).json({ message: error.message });
+          } else if (errorCode === 'auth/weak-password') {
+            res.status(400).json({ message: error.message });
+          } else if (errorCode === 'auth/email-already-in-use') {
+            res.status(409).json({ message: error.message });
+          }
+        });
     }
   }
 
- /**
-     * The Google Sign Up method
-     * @param {number} req - User's Request
-     * @param {number} res - Server Response
-     * @return {object}  returns the user's details
-     */
+  /**
+      * The Google Sign Up method
+      * @param {number} req - User's Request
+      * @param {number} res - Server Response
+      * @return {object}  returns the user's details
+      */
   static google(req, res) {
     const googleUser = req.body.googleUser;
     const { userName, email, uid, number } = googleUser;
@@ -62,22 +62,22 @@ class User {
       email,
       uid,
       number
-    }); 
+    });
     const rootRef = firebase.database().ref()
-    .child('users')
-    .child(userName)
-    .child('Groups');
+      .child('users')
+      .child(userName)
+      .child('Groups');
     rootRef.once('value', (snap) => {
       const data = snap.val()
       const groups = []
-      let group = {}  
-      for (var i in data){
-        group = {            
+      let group = {}
+      for (var i in data) {
+        group = {
           groupName: data[i].groupName,
-          userName: data[i].userName      
+          userName: data[i].userName
         }
         groups.push(group)
-       }  
+      }
 
       res.status(200).send({
         message: 'Welcome to Post it app',
@@ -88,64 +88,64 @@ class User {
   }
 
 
- /**
-     * The Sign In method
-     * @param {number} req - User's Request
-     * @param {object} res - Server Response
-     * @return {object}  returns the user's details
-     */
+  /**
+      * The Sign In method
+      * @param {number} req - User's Request
+      * @param {object} res - Server Response
+      * @return {object}  returns the user's details
+      */
   static signin(req, res) {
-    const { email, password } = req.body;   
-    if (typeof email === 'undefined' || typeof password === 'undefined' ) {
+    const { email, password } = req.body;
+    if (typeof email === 'undefined' || typeof password === 'undefined') {
       res.status(400).json({ message: 'You need to provide password and email' });
-    } else if (email === '' || password === '' ) {
+    } else if (email === '' || password === '') {
       res.status(400).json({ message: 'Email or Password cannot be empty' });
     } else {
       firebase.auth()
-    .signInWithEmailAndPassword(email, password).then((user) => {
-      const userName = user.displayName;     
-      // Get all user's personal message while signing in
-      const rootRef = firebase.database().ref().child('users').child(userName)
-      .child('Groups');
-      rootRef.once('value', (snap) => {
-        const groups = [];
-        let group = {}
-        const groupRef = firebase.database().ref().child('users').child(userName).child('Groups');
-          groupRef.once('value', (snap) => {
-          snap.forEach((data) => {
-            group = {  
-              groupName: data.val().groupName
-            };
-            groups.push(group)
+        .signInWithEmailAndPassword(email, password).then((user) => {
+          const userName = user.displayName;
+          // Get all user's personal message while signing in
+          const rootRef = firebase.database().ref().child('users').child(userName)
+            .child('Groups');
+          rootRef.once('value', (snap) => {
+            const groups = [];
+            let group = {}
+            const groupRef = firebase.database().ref().child('users').child(userName).child('Groups');
+            groupRef.once('value', (snap) => {
+              snap.forEach((data) => {
+                group = {
+                  groupName: data.val().groupName
+                };
+                groups.push(group)
+              });
+              res.status(200).send({
+                message: 'Welcome to Post it app',
+                userData: user,
+                groups
+              });
+            });
           });
-          res.status(200).send({
-            message: 'Welcome to Post it app',
-            userData: user,
-            groups
-          });
-        });           
-      });
-    })
-    .catch((error) => {
-      const errorCode = error.code;
-      if (errorCode === 'auth/invalid-email') {
-        res.status(400).json({ message: error.message }); 
-      } else if(errorCode === 'auth/user-not-found'){
-          res.status(401).json({ message: error.message });
-       } else if (errorCode === 'auth/wrong-password') {
-        res.status(401).json({ message: error.message }); 
-       
-      }
-    });
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          if (errorCode === 'auth/invalid-email') {
+            res.status(400).json({ message: error.message });
+          } else if (errorCode === 'auth/user-not-found') {
+            res.status(401).json({ message: error.message });
+          } else if (errorCode === 'auth/wrong-password') {
+            res.status(401).json({ message: error.message });
+
+          }
+        });
     }
   }
 
- /**
-     * The Sign Out method
-     * @param {null} req - User's Request
-     * @param {object} res - Server Response
-     * @return {object}  returns the user's details
-     */
+  /**
+      * The Sign Out method
+      * @param {null} req - User's Request
+      * @param {object} res - Server Response
+      * @return {object}  returns the user's details
+      */
   static signout(req, res) {
     firebase.auth().signOut().then(() => {
       res.status(200).send({
@@ -159,51 +159,51 @@ class User {
   }
 
 
-static notification(req, res){
-firebase.auth().onAuthStateChanged((user) => {
+  static notification(req, res) {
+    firebase.auth().onAuthStateChanged((user) => {
       if (user) {
         // This means a user is signed in
         const userId = user.uid;
         const rootRef = firebase.database().ref().child('users');
-    rootRef.once('value', snap => {
-      const data = snap.val() 
-      for (var i in data){           
-        if (userId == data[i].uid){     
-          var notification = data[i].Notifications              
-        }      
-        }  
-   res.send(notification) 
-    })
+        rootRef.once('value', snap => {
+          const data = snap.val()
+          for (var i in data) {
+            if (userId == data[i].uid) {
+              var notification = data[i].Notifications
+            }
+          }
+          res.send(notification)
+        })
       } else {
         console.log({
           // user is not signed in
           message: 'You are not signed in right now!'
-        });       
+        });
       }
-    });  
+    });
   }
 
   //Get All Users in the Database
-  static allUsers(req, res){
+  static allUsers(req, res) {
     const rootRef = firebase.database().ref().child('users');
     rootRef.once('value', (snap) => {
       const data = snap.val();
       const users = [];
-      for(var i in data) {
+      for (var i in data) {
         users.push(data[i].username);
       }
       res.send(users);
     });
   }
 
- 
- //Get All Phone Numbers in the database
-    static allNumbers(req, res){
+
+  //Get All Phone Numbers in the database
+  static allNumbers(req, res) {
     const rootRef = firebase.database().ref().child('users');
     rootRef.once('value', (snap) => {
       const data = snap.val();
       const numbers = [];
-      for(var i in data) {
+      for (var i in data) {
         numbers.push(data[i].number);
       }
       res.status(200).send(numbers);
@@ -221,21 +221,18 @@ firebase.auth().onAuthStateChanged((user) => {
   //   })   
   // })
 
-
-
-
-  static resetPassword(req, res) {    
-    const emailAddress = req.body.email 
+  static resetPassword(req, res) {
+    const emailAddress = req.body.email
     var auth = firebase.auth();
-    auth.sendPasswordResetEmail(emailAddress).then(function() {
-     res.status(201).json({ message: 'An email has been sent for password reset. Log in after Reset' }); 
-    }, function(error) {
+    auth.sendPasswordResetEmail(emailAddress).then(function () {
+      res.status(201).json({ message: 'An email has been sent for password reset. Log in after Reset' });
+    }, function (error) {
       const errorCode = error.code;
       if (errorCode === 'auth/invalid-email') {
-        res.status(400).json({ message: error.message }); 
-      } else if(errorCode === 'auth/user-not-found'){
-          res.status(401).json({ message: error.message });
-       } 
+        res.status(400).json({ message: error.message });
+      } else if (errorCode === 'auth/user-not-found') {
+        res.status(401).json({ message: error.message });
+      }
     });
   }
 
